6a6637a8611e4ce115ba1802c251d25eea36e74e
KangMinSoo <danny7742@naver.com>
2018-11-09 17:28:34 +0900

    getintvNum modify
    getintvNum modify

--- VOICEofCAU/app/src/main/java/com/team4/capstone/voiceofcau/AudioController.java
+++ VOICEofCAU/app/src/main/java/com/team4/capstone/voiceofcau/AudioController.java
@@ -313,127 +313,13 @@ class  MyPitchDetector implements PitchDetectionHandler{
             int pMinute = (int) (timeStamp/60);
             int pSecond = (int) (timeStamp%60);
             int intvNum = -1;
-            String tInterval = new String();
-            if(pitch >= 127.142 && pitch < 134.702) {
-                tInterval = "C3";
-                intvNum = 1;
-            }
-            else if(pitch >= 134.702 && pitch < 142.712) {
-                tInterval = "C3#";
-                intvNum = 2;
-            }
-            else if(pitch >= 142.712 && pitch < 151.198) {
-                tInterval = "D3";
-                intvNum=3;
-            }
-            else if(pitch >= 151.198 && pitch < 160.189) {
-                tInterval = "D3#";
-                intvNum=4;
-            }
-            else if(pitch >= 160.189 && pitch < 169.714) {
-                tInterval = "E3";
-                intvNum=5;
-            }
-            else if(pitch >= 169.714 && pitch < 179.806) {
-                tInterval = "F3";
-                intvNum=6;
-            }
-            else if(pitch >= 179.806 && pitch < 190.497) {
-                tInterval = "F3#";
-                intvNum=7;
-            }
-            else if(pitch >= 190.497 && pitch < 201.825) {
-                tInterval = "G3";
-                intvNum=8;
-            }
-            else if(pitch >= 201.825 && pitch < 213.826) {
-                tInterval = "G3#";
-                intvNum=9;
-            }
-            else if(pitch >= 213.826 && pitch < 226.541) {
-                tInterval = "A3";
-                intvNum=10;
-            }
-            else if(pitch >= 226.541 && pitch < 240.012) {
-                tInterval = "A3#";
-                intvNum=11;
-            }
-            else if(pitch >= 240.012 && pitch < 254.284) {
-                tInterval = "B3";
-                intvNum=12;
-            }
-            else if(pitch >= 254.284 && pitch < 269.404) {
-                tInterval = "C4";
-                intvNum=13;
-            }
-            else if(pitch >= 269.404 && pitch < 287.924) {
-                tInterval = "C4#";
-                intvNum=14;
-            }
-            else if(pitch >= 287.294 && pitch < 302.396) {
-                tInterval = "D4";
-                intvNum=15;
-            }
-            else if(pitch >= 302.396 && pitch < 320.377) {
-                tInterval = "D4#";
-                intvNum=16;
-            }
-            else if(pitch >= 320.377 && pitch < 339.428) {
-                tInterval = "E4";
-                intvNum=17;
-            }
-            else if(pitch >= 339.428 && pitch < 359.611) {
-                tInterval = "F4";
-                intvNum=18;
-            }
-            else if(pitch >= 359.611 && pitch < 380.995) {
-                tInterval = "F4#";
-                intvNum=19;
-            }
-            else if(pitch >= 380.995 && pitch < 403.65) {
-                tInterval = "G4";
-                intvNum=20;
-            }
-            else if(pitch >= 403.65 && pitch < 427.652) {
-                tInterval = "G4#";
-                intvNum=21;
-            }
-            else if(pitch >= 427.652 && pitch < 453.082) {
-                tInterval = "A4";
-                intvNum=22;
-            }
-            else if(pitch >= 453.082 && pitch < 480.234) {
-                tInterval = "A4#";
-                intvNum=23;
-            }
-            else if(pitch >= 480.234 && pitch < 508.567) {
-                tInterval = "B4";
-                intvNum=24;
-            }
-            else if(pitch >= 508.567 && pitch < 538.808) {
-                tInterval = "C5";
-                intvNum=25;
-            }
-            else if(pitch >= 538.808 && pitch < 570.847) {
-                tInterval = "C5#";
-                intvNum=26;
-            }
-            else if(pitch >= 570.847 && pitch < 604.792) {
-                tInterval = "D5";
-                intvNum=27;
-            }
-            else if(pitch >= 604.792 && pitch < 640.755) {
-                tInterval = "D5#";
-                intvNum=28;
-            }
-            else {
-                tInterval = "null";
-                intvNum=-1;
-            }
+
+            intvNum = getintvNum(pitch);
+
             //Converting Ended
             if(pitch < 8000) {
-                String message = String.format("Pitch detected at %d 분 %d초, %.2f: %.2fHz ( %.2f probability) %s",
-                        pMinute, pSecond, timeStamp, pitch,probability, tInterval);
+                String message = String.format("Pitch detected at %d 분 %d초, %.2f: %.2fHz ( %.2f probability)",
+                        pMinute, pSecond, timeStamp, pitch,probability);
                 Log.d("test", message);
                 if(intvNum!=-1) {
                     CalculateScore.Time.add(timeStamp);
@@ -442,8 +328,100 @@ class  MyPitchDetector implements PitchDetectionHandler{
             }
         }
     }
-}
 
+    private int getintvNum(float pitch)
+    {
+        int intvNum = 0;
+        if(pitch >= 127.142 && pitch < 134.702) {
+            intvNum = 1;
+        }
+        else if(pitch >= 134.702 && pitch < 142.712) {
+            intvNum = 2;
+        }
+        else if(pitch >= 142.712 && pitch < 151.198) {
+            intvNum=3;
+        }
+        else if(pitch >= 151.198 && pitch < 160.189) {
+            intvNum=4;
+        }
+        else if(pitch >= 160.189 && pitch < 169.714) {
+            intvNum=5;
+        }
+        else if(pitch >= 169.714 && pitch < 179.806) {
+            intvNum=6;
+        }
+        else if(pitch >= 179.806 && pitch < 190.497) {
+            intvNum=7;
+        }
+        else if(pitch >= 190.497 && pitch < 201.825) {
+            intvNum=8;
+        }
+        else if(pitch >= 201.825 && pitch < 213.826) {
+            intvNum=9;
+        }
+        else if(pitch >= 213.826 && pitch < 226.541) {
+            intvNum=10;
+        }
+        else if(pitch >= 226.541 && pitch < 240.012) {
+            intvNum=11;
+        }
+        else if(pitch >= 240.012 && pitch < 254.284) {
+            intvNum=12;
+        }
+        else if(pitch >= 254.284 && pitch < 269.404) {
+            intvNum=13;
+        }
+        else if(pitch >= 269.404 && pitch < 287.924) {
+            intvNum=14;
+        }
+        else if(pitch >= 287.294 && pitch < 302.396) {
+            intvNum=15;
+        }
+        else if(pitch >= 302.396 && pitch < 320.377) {
+            intvNum=16;
+        }
+        else if(pitch >= 320.377 && pitch < 339.428) {
+            intvNum=17;
+        }
+        else if(pitch >= 339.428 && pitch < 359.611) {
+            intvNum=18;
+        }
+        else if(pitch >= 359.611 && pitch < 380.995) {
+            intvNum=19;
+        }
+        else if(pitch >= 380.995 && pitch < 403.65) {
+            intvNum=20;
+        }
+        else if(pitch >= 403.65 && pitch < 427.652) {
+            intvNum=21;
+        }
+        else if(pitch >= 427.652 && pitch < 453.082) {
+            intvNum=22;
+        }
+        else if(pitch >= 453.082 && pitch < 480.234) {
+            intvNum=23;
+        }
+        else if(pitch >= 480.234 && pitch < 508.567) {
+            intvNum=24;
+        }
+        else if(pitch >= 508.567 && pitch < 538.808) {
+            intvNum=25;
+        }
+        else if(pitch >= 538.808 && pitch < 570.847) {
+            intvNum=26;
+        }
+        else if(pitch >= 570.847 && pitch < 604.792) {
+            intvNum=27;
+        }
+        else if(pitch >= 604.792 && pitch < 640.755) {
+            intvNum=28;
+        }
+        else {
+            intvNum=-1;
+        }
+        return intvNum;
+    }
+}
 
 class CalculateScore {
     public static ArrayList<Double> Time = new ArrayList<>();
