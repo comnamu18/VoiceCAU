1309145eb4bdf607f15213fd5f00e21377758fd5
KangMinSoo <danny7742@naver.com>
2018-11-27 00:57:31 +0900

    Revert "Revert "Canvas Test""
    This reverts commit b0da394e7de59117d12071ec07fd0587674614a1.

--- VOICEofCAU/app/src/main/java/com/team4/capstone/voiceofcau/StatisticActivity.java
+++ VOICEofCAU/app/src/main/java/com/team4/capstone/voiceofcau/StatisticActivity.java
@@ -2,8 +2,15 @@ package com.team4.capstone.voiceofcau;
 
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.mobile.client.AWSMobileClient;
@@ -20,15 +27,22 @@ import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
 import com.amazonaws.services.dynamodbv2.model.Condition;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+
 
 public class StatisticActivity extends AppCompatActivity {
     DynamoDBMapper dynamoDBMapper;
     AWSCredentialsProvider credentialsProvider;
     String UserID;
+    boolean sema = false;
+    ListView listview ;
+    StatisticAdapter adapter;
+    ArrayList<String[]>  datas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         UserID = getIntent().getStringExtra("USERID");
-
+        datas = new ArrayList<String[]>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
         credentialsProvider = AWSMobileClient.getInstance().getCredentialsProvider();
@@ -41,8 +55,44 @@ public class StatisticActivity extends AppCompatActivity {
                 .awsConfiguration(configuration)
                 .build();
 
-        Log.d("STAT", "STARTING");
+        // Adapter 생성
+        adapter = new StatisticAdapter() ;
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.listview2);
+        listview.setAdapter(adapter);
         getScoreStat();
+
+        while (!sema){{
+        }}
+        Log.d("Out", String.valueOf(datas.size()));
+        for(int i = 0; i < datas.size(); i++) {
+            String[] datas1 = datas.get(i);
+            String date = new String(datas1[3]);
+            String score = new String(datas1[7]);
+            String songname = new String(datas1[11]);
+            String userid = new String(datas1[15]);
+
+            Log.d("date result: ", date);
+            Log.d("score result: ", score);
+            Log.d("songname result: ", songname );
+            Log.d("userid result: ", userid );
+            adapter.addItem(userid, songname, score, date) ;
+        }
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                StatisticItem item = (StatisticItem) parent.getItemAtPosition(position) ;
+
+                String dateStr = item.getdate() ;
+                String scoreStr = item.getscore() ;
+                String songnameStr = item.getsongname() ;
+                String useridStr = item.getuserid() ;
+
+            }
+        }) ;
     }
 
     public void getScoreStat() {
@@ -64,7 +114,11 @@ public class StatisticActivity extends AppCompatActivity {
                 // Loop through query results
                 for (int i = 0; i < result.size(); i++) {
                     String jsonFormOfItem = gson.toJson(result.get(i));
-                    stringBuilder.append(jsonFormOfItem + "\n\n");
+                    Log.d("TESTJSON", jsonFormOfItem);
+                    String[] a = jsonFormOfItem.split("\"");
+                    Log.d("TESTJSON", a[3]);
+                    datas.add(jsonFormOfItem.split("\""));
+                    Log.d("datasLenght", String.valueOf(datas.size()));
                 }
 
                 // Add your code here to deal with the data result
@@ -73,6 +127,7 @@ public class StatisticActivity extends AppCompatActivity {
                 if (result.isEmpty()) {
                     // There were no items matching your query.
                 }
+                sema = true;
             }
         }).start();
     }
--- VoiceOfCAU/app/src/main/java/com/team4/capstone/voiceofcau/MainActivity.java
+++ VoiceOfCAU/app/src/main/java/com/team4/capstone/voiceofcau/MainActivity.java
@@ -228,6 +228,7 @@ public class MainActivity extends AppCompatActivity
                                 break;
 
                             case R.id.action_stat:
+                                /*
                                 String root = Environment.getExternalStorageDirectory().toString();
                                 String audio = root + "/sample.m4a";
                                 String video = root + "/everytime.mp4";
@@ -236,6 +237,7 @@ public class MainActivity extends AppCompatActivity
                                 Log.e("FILE", "audio:"+audio + " video:"+video+ " out:"+output);
                                 TestOverLay test = new TestOverLay(testF);
                                 test.mux(video, audio, output);
+                                */
                                 intent = new Intent(getApplicationContext(), StatisticActivity.class);
                                 intent.putExtra("USERID", UserID);
                                 startActivity(intent);
